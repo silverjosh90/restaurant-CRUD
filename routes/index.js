67bb4a70147d4b7feca8cdb738522f13f6a54ffc
Josh Silverstein <silverjosh90@gmail.com>
2016-01-12 09:23:52 -0700

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,76 @@ var express = require('express');
 var router = express.Router();
 var pg = require('pg');
 var app = express();
+var server = require('../serverlogic/servlogic.js');
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'gTables' });
+  server.runQuery('SELECT * FROM foods', function(results){
+    var result = results.rows
+    res.render('index', { title: 'gTables', rest: result });
+  })
 });
+
+router.get('/new', function(req,res,next){
+  res.render('restaurant', {state: server.states, cuisine: server.cuisine })
+
+})
+
+router.post("/add", function(req,res){
+  if(req.body.submit) {
+    server.postAdd(req.body.name, req.body.location, req.body.states, req.body.type, req.body.image, req.body.rating, req.body.bio)
+
+}
+  res.redirect('/')
+})
+
+router.get("/:title", function(req,res) {
+
+  server.runQuery('SELECT * FROM foods WHERE name=' + "'" + req.params.title + "'", function(results) {
+    var restInfo = results.rows[0]
+    res.render('indRest', {title: restInfo})
+  });
+});
+
+router.get("/edit/:title", function(req,res){
+  server.runQuery('SELECT * FROM foods WHERE name=' + "'" + req.params.title + "'", function(results) {
+    var restaurants = results.rows[0]
+
+    res.render('editRest', {rest: req.params.title, cuisine: server.cuisine, state: server.states, restaurant: restaurants})
+
+  })
+})
+
+router.post("/edit/:title", function(req,res){
+  // var j = server.postUpdate(req.body.name, req.body.location, req.body.states, req.body.type, req.body.image, req.body.rating, req.body.bio)
+
+  server.runQuery('SELECT * FROM foods WHERE name=' + "'" + req.params.title + "'", function(resulted) {
+    var d = resulted.rows[0]
+    var ided = d.id
+    var rating = Number(req.body.rating)
+
+   server.runQuery("UPDATE foods SET name='"+req.body.name+"',location='"+req.body.location+"', state='"+req.body.states+"', type='"+req.body.type+"', image='"+req.body.image+"',rating='"+rating+"',bio='"+req.body.bio+"' WHERE id='"+ided+"'", function(results) {
+
+     res.redirect("/")
+   })
+
+ })
+
+  // server.runQuery(j, function(results) {
+  //   console.log("Worked!!")
+  // });
+})
+
+
+
+router.post("/delete/:title", function(req,res){
+  server.runQuery('DELETE FROM foods WHERE name='+"'"+req.params.title+"'", function(results){
+    console.log("Good job!");
+    res.redirect("/")
+  })
+})
+
+
 
 module.exports = router;
--- serverlogic/servlogic.js
+++ serverlogic/servlogic.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var pg = require('pg');
-var server = require('../serverlogic/servlogic.js');
-var connectionString = 'postgres://localhost/library';
+var connectionString = 'postgres://localhost/restaurant';
 var app = express();
+var knex = require('knex')
 
 function runQuery (query,callback){
    pg.connect(connectionString, function(err, client, done) {
@@ -16,7 +16,67 @@ function runQuery (query,callback){
    });
  }
 
-module.exports = {
-  runQuery: runQuery;
+ function postAdd(name, location, state, type, image, rating, bio) {
+   var review = Number(rating)
+   var j = "INSERT INTO foods VALUES(default,'"+name+"','"+location+"','"+state+"','"+type+"','"+image+"','"+review+"','"+bio+"');"
+   runQuery(j, function(results){
+   })
+ }
 
+ function postUpdate(name, location, state, type, image, rating, bio, id) {
+   var review = Number(rating)
+   var j = "UPDATE foods SET name='"+name+"',location='"+location+"', state='"+state+"', type'"+type+"', image='"+image+"',review='"+review+"',bio='"+bio+"' WHERE id='"+id+"'"
+
+   console.log("UPDATE foods SET name='"+name+"',location='"+location+"', state='"+state+"', type'"+type+"', image='"+image+"',review='"+review+"',bio='"+bio+"' WHERE id='"+id+"'")
+   runQuery(j, function(results){
+     return results
+   })
+ }
+
+ // function removeIllegals(name,location,image, bio) {
+ //   var array = [name,location,image,bio]
+ //   var newArray = []
+ //   for (var i = 0; i < array.length; i++) {
+ //     var word = array[i]
+ //      for (var i = 0; i < word.length; i++) {
+ //        var d = ""
+ //        if(word[i] == "'" || word[i] == '"') {
+ //          d += "\'"
+ //        }
+ //        else {
+ //          d+= word[i]
+ //        }
+ //      }
+ //   }
+ // }
+
+
+
+
+ function postSelect() {
+   runQuery('SELECT * FROM foods', function(results){
+     var result = results.rows
+     return result
+   })
+ }
+
+var states = [ 'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI',
+  'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI',
+  'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC',
+  'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT',
+  'VT', 'VA', 'WA', 'WV', 'WI', 'AL', 'AK', 'AZ', 'AR', 'CA', 'CO',
+  'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY',
+  'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV',
+  'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI',
+  'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY' ]
+
+  var cuisine = ['Italian','Mexican','Thai','French','Vietnames','Chinese','American','Other']
+
+module.exports = {
+  runQuery: runQuery,
+  states: states,
+  cuisine: cuisine,
+  postAdd: postAdd,
+  postSelect: postSelect,
+  postUpdate: postUpdate
 }
