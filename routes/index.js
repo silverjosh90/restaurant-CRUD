755286bf44529c0fcf959607bebf46b7b252b594
Josh Silverstein <silverjosh90@gmail.com>
2016-01-18 09:30:16 -0700

    fixed employee bug

--- routes/index.js
+++ routes/index.js
@@ -31,10 +31,11 @@ router.get("/:title", function(req,res) {
 
   server.runQuery('SELECT * FROM foods WHERE name=' + "'" + req.params.title + "'", function(results) {
     var restInfo = results.rows[0];
+    console.log(restInfo);
     var query='SELECT * from reviews WHERE restaurant_id='+restInfo.id;
   server.runQuery(query, function(rest) {
     var reviewed = rest.rows
-    console.log(reviewed);
+
     res.render('indRest', {title: restInfo, header: req.params.title, reviews: reviewed})
     });
   });
@@ -61,14 +62,15 @@ router.get("/restaurant/admin", function(req,res) {
 });
 
 router.post("/restaurant/employees/add", function(req,res){
-  var restaurantName = req.body.restaurantName
   var first = req.body.first_name
   var last = req.body.last_name
+  var position = req.body.position
+  var restaurantName = req.body.restaurantName
   server.runQuery("SELECT id from foods WHERE name='" + restaurantName+"'", function(results){
     var rows = results.rows[0]
     var ided = rows.id
 
-  var query = "INSERT INTO employees VALUES(default,'"+first+"','" + last + "'," + ided + " );"
+  var query = "INSERT INTO employees VALUES(default,'"+first+"','" + last + "'," + ided + ",'"+position+"');"
   server.runQuery(query, function(results) {
     res.redirect('/');
   })
